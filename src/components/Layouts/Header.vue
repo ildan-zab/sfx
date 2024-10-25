8b1809a3589e018e46cc5144774682ca29d5bf9e
NoName <ildanzabir@gmail.com>
2024-10-25 22:38:34 +0300

    add layouts components

---
+++ src/components/Layouts/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header__logo">
+        <NuxtLink class="header__logo-link" to="/">
+          <nuxt-icon class="header__logo-icon" name="logo" filled />
+        </NuxtLink>
+      </div>
+    </div>
+  </header>
+</template>
