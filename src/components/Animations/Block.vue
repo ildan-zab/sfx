c1d8a42b4d8af9f841452e32be6361887e0245cb
NoName <ildanzabir@gmail.com>
2024-10-25 23:00:34 +0300

    add animations components

---
+++ src/components/Animations/Block.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="animations-block">
+    <AnimationsLoader />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.animations-block {
+  position: absolute;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #00000024;
+}
+</style>
---
+++ src/components/Animations/Loader.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="animations-loader"></span>
+</template>
+
+<style lang="scss" scoped>
+.animations-loader {
+  width: 15px;
+  height: 15px;
+  border: 2px solid $color-text;
+  margin-right: 10px;
+  border-bottom-color: transparent;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
