166be34915d70f85f7a13d8a3b907a204876804c
NoName <ildanzabir@gmail.com>
2024-10-26 11:43:19 +0300

    add local styles component

--- src/components/Post/Dislike.vue
+++ src/components/Post/Dislike.vue
@@ -11,3 +11,37 @@ const { dislikes } = defineProps<{
   dislikes: Number;
 }>();
 </script>
+
+<style lang="scss" scoped>
+.post-dislike {
+  padding: 8px 8px 8px 12px;
+  border-radius: 0px 30px 30px 0px;
+  color: #040405;
+  background-color: #0404050a;
+  display: inline-flex;
+  align-items: center;
+  transition: all cubic-bezier(0.3, 0, 1, 1) 0.1s;
+
+  &:hover,
+  &--active {
+    color: $color-white;
+    background-color: #040405;
+  }
+
+  &__icon {
+    width: 13px;
+    fill: #040405;
+  }
+
+  &__text {
+    margin-left: 4px;
+    font-size: $font-size-caption;
+  }
+
+  &__count {
+    margin-left: 6px;
+    font-size: $font-size-numtub;
+    color: #acacad;
+  }
+}
+</style>
--- src/components/Post/Like.vue
+++ src/components/Post/Like.vue
@@ -11,3 +11,42 @@ const { likes } = defineProps<{
   likes: Number;
 }>();
 </script>
+
+<style lang="scss" scoped>
+.post-like {
+  padding: 8px 8px 8px 12px;
+  border-radius: 30px 0px 0px 30px;
+  color: #040405;
+  background-color: #0404050a;
+  display: inline-flex;
+  align-items: center;
+  transition: all cubic-bezier(0.3, 0, 1, 1) 0.1s;
+
+  &:hover,
+  &--active {
+    color: $color-white;
+    background-color: $color-error;
+
+    .post-dislike__count,
+    .post-like__count {
+      color: $color-white;
+    }
+  }
+
+  &__icon {
+    width: 13px;
+    fill: #040405;
+  }
+
+  &__text {
+    margin-left: 4px;
+    font-size: $font-size-caption;
+  }
+
+  &__count {
+    margin-left: 6px;
+    font-size: $font-size-numtub;
+    color: #acacad;
+  }
+}
+</style>
